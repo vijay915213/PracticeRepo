28ca05ce86517c414746afd2871ebff7b03f2171
Vijay <vijay.k@jarustech.com>
2014-01-09 19:48:22 +0530

    samplefileOne.c

---
+++ samplefileOne.c
@@ -0,0 +1,35 @@
+#include stdio.h
+#include myheader.h
+
+void main(void) {
+Let us do some stuff here....
+make a function call
+hello();
+make another function call
+world();
+
+do some processing
+okay - are we done ?
+if ( wearedone )
+  get out 
+  exit;
+
+do some thing more
+return ;
+}
+
+hello() {
+aaha! we are now in a function
+process...
+process...
+process...
+
+done processing
+}
+
+world() {
+
+throw error 
+too big to process...
+
+}
